96f02b4e8d4eb654a83c1d1b3d81699c3a0a0611
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2021-11-06 13:16:21 -0400

    Initial Commit

---
+++ Shared/IdleFarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdleFarmApp.swift
+//  Shared
+//
+//  Created by AlecNipp on 11/6/21.
+//
+
+import SwiftUI
+
+@main
+struct IdleFarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
